7684fad1f5d2e6344715f0782d5e39605839555a
YogeshwariDubale <152952420+YogeshwariDubale@users.noreply.github.com>
2024-07-05 09:35:23 +0530

    Update AdminService.java

--- library/service/AdminService.java
+++ library/service/AdminService.java
@@ -4,6 +4,6 @@ import com.yogeshwari.library.entity.Admin;
 
 public interface AdminService {
 	String registerAdmin(Admin admin);
-	 Admin getAdminById(Integer adminId);
-    boolean adminLogin(Integer adminId,String adminPassword);
+	Admin getAdminById(Integer adminId);
+        boolean adminLogin(Integer adminId,String adminPassword);
 }
