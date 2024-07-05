3bc2a768b62ef2b8a82f9c8b2044d82dde73d895
YogeshwariDubale <152952420+YogeshwariDubale@users.noreply.github.com>
2024-07-05 09:40:32 +0530

    Update StudentDao.java

--- library/dao/StudentDao.java
+++ library/dao/StudentDao.java
@@ -6,8 +6,8 @@ import com.yogeshwari.library.entity.Student;
 public interface StudentDao {
 	String registerStudent(Student student);
 	public Student getStudentById(Integer studentId);
-    boolean studentLogin(Integer studentId,String studentPassword);
-    List<Student> getAllStudent();
-    List<Student> getStudentByName(String studentName);
+        boolean studentLogin(Integer studentId,String studentPassword);
+        List<Student> getAllStudent();
+        List<Student> getStudentByName(String studentName);
 
 }
