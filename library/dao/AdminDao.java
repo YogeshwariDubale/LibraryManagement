adf26b0e1fdabde26301ce7dc1267201f259c9f2
YogeshwariDubale <152952420+YogeshwariDubale@users.noreply.github.com>
2024-07-05 09:39:36 +0530

    Update AdminDao.java

--- library/dao/AdminDao.java
+++ library/dao/AdminDao.java
@@ -5,5 +5,5 @@ import com.yogeshwari.library.entity.Admin;
 public interface AdminDao {
 	String registerAdmin(Admin admin);
 	public Admin getAdminById(Integer adminId);
-    boolean adminLogin(Integer adminId,String adminPassword);
+        boolean adminLogin(Integer adminId,String adminPassword);
 }
